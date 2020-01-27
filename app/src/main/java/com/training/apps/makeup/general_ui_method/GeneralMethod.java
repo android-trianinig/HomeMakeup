8f2b0f4083514370d784c8f98cc90d4d6744ca7c
Ahmed Ramadan <AhmedShouman53@gmail.com>
2020-01-27 04:02:52 +0200

    update the project with all packages

---
+++ app/src/main/java/com/training/apps/makeup/general_ui_method/GeneralMethod.java
@@ -0,0 +1,17 @@
+package com.training.apps.makeup.general_ui_method;
+
+public class GeneralMethod {
+
+//    @BindingAdapter("error")
+//    public static void errorValidation(View view, String error) {
+//        if (view instanceof EditText) {
+//            EditText ed = (EditText) view;
+//            ed.setError(error);
+//        } else if (view instanceof TextView) {
+//            TextView tv = (TextView) view;
+//            tv.setError(error);
+//
+//
+//        }
+}
+
---
+++ app/src/main/java/com/training/apps/makeup/tags/Tags.java
@@ -0,0 +1,10 @@
+package com.training.apps.makeup.tags;
+
+public class Tags {
+    public static String base_url = "hhhhhh";
+
+
+    public static final String session_login = "login";
+    public static final String session_logout = "logout";
+
+}
